f7cd47c64a7e9e6d5d27895771661a02c24c0ea9
Rob Ramsay <robert.ramsay.nz@gmail.com>
2014-11-26 00:09:49 +1300

    add bourbon/neat/bitters to the project

--- _source/live-update.sh
+++ _source/live-update.sh
@@ -3,7 +3,13 @@
 if [ "$1" = "--run-once" ] ;then
    # Commands
    haml ./index.haml > ../index.html
+   haml ./about.haml > ../index.html
+
+   rsync -ai a.html ../a.html
+
+   scss stylesheets/a.css.scss > ../stylesheets/a.css
    rsync -ai ./css/ ../css/
+
    rsync -ai ./images/ ../images/
 
    echo "site updated."
